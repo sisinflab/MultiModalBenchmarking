92525d70e53d68cb5fc7dba53788c87f58f53d19
daniele <d.malitesta@gmail.com>
2024-06-13 11:12:49 +0200

    Added config files for new multimodal models

--- config_templates.py
+++ config_templates.py
@@ -1,5 +1,92 @@
 import argparse
 
+# config_elliot = """experiment:
+#   backend: pytorch
+#   data_config:
+#     strategy: fixed
+#     train_path: ../data/{{0}}/train_indexed.tsv
+#     validation_path: ../data/{{0}}/val_indexed.tsv
+#     test_path: ../data/{{0}}/test_indexed.tsv
+#     side_information:
+#       - dataloader: VisualAttribute
+#         visual_features: ../data/{{0}}/visual_embeddings_indexed_{batch_size}/{visual_path}
+#       - dataloader: TextualAttribute
+#         textual_features: ../data/{{0}}/textual_embeddings_indexed_{batch_size}/{textual_path}
+#   dataset: {dataset}
+#   top_k: 50
+#   evaluation:
+#     cutoffs: [ 10, 20, 50 ]
+#     simple_metrics: [ Recall, Precision, nDCG, HR ]
+#   gpu: 0
+#   external_models_path: ../external/models/__init__.py
+#   models:
+#     external.VBPR:
+#       meta:
+#         hyper_opt_alg: grid
+#         verbose: True
+#         save_weights: False
+#         save_recs: False
+#         validation_rate: 10
+#         validation_metric: Recall@20
+#         restore: False
+#       lr: [ 0.0001, 0.0005, 0.001, 0.005, 0.01 ]
+#       modalities: ('visual', 'textual')
+#       epochs: 200
+#       factors: 64
+#       batch_size: 1024
+#       l_w: [ 1e-5, 1e-2 ]
+#       comb_mod: concat
+#       loaders: ('VisualAttribute', 'TextualAttribute')
+#       seed: 123
+#     external.BM3:
+#       meta:
+#         hyper_opt_alg: grid
+#         verbose: True
+#         save_weights: False
+#         save_recs: False
+#         validation_rate: 10
+#         validation_metric: Recall@20
+#         restore: False
+#       lr: [ 0.0001, 0.0005, 0.001, 0.005, 0.01 ]
+#       multimod_factors: 64
+#       reg_weight: [ 0.1, 0.01 ]
+#       cl_weight: 2.0
+#       dropout: 0.3
+#       n_layers: 2
+#       modalities: ('visual', 'textual')
+#       loaders: ('VisualAttribute', 'TextualAttribute')
+#       epochs: 200
+#       factors: 64
+#       lr_sched: (1.0,50)
+#       batch_size: 1024
+#       seed: 123
+#     external.FREEDOM:
+#       meta:
+#         hyper_opt_alg: grid
+#         verbose: True
+#         save_weights: False
+#         save_recs: False
+#         validation_rate: 10
+#         validation_metric: Recall@20
+#         restore: False
+#       lr: [ 0.0001, 0.0005, 0.001, 0.005, 0.01 ]
+#       factors: 64
+#       epochs: 200
+#       l_w: [ 1e-5, 1e-2 ]
+#       n_layers: 1
+#       n_ui_layers: 2
+#       top_k: 10
+#       factors_multimod: 64
+#       modalities: ('visual', 'textual')
+#       loaders: ('VisualAttribute', 'TextualAttribute')
+#       mw: (0.1,0.9)
+#       drop: 0.8
+#       lr_sched: (1.0,50)
+#       batch_size: 1024
+#       seed: 123
+#
+# """
+
 config_elliot = """experiment:
   backend: pytorch
   data_config:
@@ -13,14 +100,14 @@ config_elliot = """experiment:
       - dataloader: TextualAttribute
         textual_features: ../data/{{0}}/textual_embeddings_indexed_{batch_size}/{textual_path}
   dataset: {dataset}
-  top_k: 50
+  top_k: 20
   evaluation:
-    cutoffs: [ 10, 20, 50 ]
+    cutoffs: [ 20 ]
     simple_metrics: [ Recall, Precision, nDCG, HR ]
   gpu: 0
   external_models_path: ../external/models/__init__.py
   models:
-    external.VBPR:
+    external.NGCFM:
       meta:
         hyper_opt_alg: grid
         verbose: True
@@ -29,38 +116,20 @@ config_elliot = """experiment:
         validation_rate: 10
         validation_metric: Recall@20
         restore: False
-      lr: [ 0.0001, 0.0005, 0.001, 0.005, 0.01 ]
-      modalities: ('visual', 'textual')
+      lr:  [ 0.0001, 0.0005, 0.001, 0.005, 0.01 ]
       epochs: 200
+      n_layers: 3
       factors: 64
+      weight_size: 64
+      node_dropout: 0.1
+      message_dropout: 0.1
       batch_size: 1024
-      l_w: [ 1e-5, 1e-2 ]
-      comb_mod: concat
-      loaders: ('VisualAttribute', 'TextualAttribute')
-      seed: 123
-    external.BM3:
-      meta:
-        hyper_opt_alg: grid
-        verbose: True
-        save_weights: False
-        save_recs: False
-        validation_rate: 10
-        validation_metric: Recall@20
-        restore: False
-      lr: [ 0.0001, 0.0005, 0.001, 0.005, 0.01 ]
-      multimod_factors: 64
-      reg_weight: [ 0.1, 0.01 ]
-      cl_weight: 2.0
-      dropout: 0.3
-      n_layers: 2
       modalities: ('visual', 'textual')
-      loaders: ('VisualAttribute', 'TextualAttribute')
-      epochs: 200
-      factors: 64
-      lr_sched: (1.0,50)
-      batch_size: 1024
+      loaders: ('VisualAttribute','TextualAttribute')
+      normalize: True
+      l_w: [ 1e-5, 1e-2 ]
       seed: 123
-    external.FREEDOM:
+    external.GRCN:
       meta:
         hyper_opt_alg: grid
         verbose: True
@@ -69,24 +138,43 @@ config_elliot = """experiment:
         validation_rate: 10
         validation_metric: Recall@20
         restore: False
-      lr: [ 0.0001, 0.0005, 0.001, 0.005, 0.01 ]
-      factors: 64
+      lr: [0.0001, 0.001, 0.01, 0.1, 1]
       epochs: 200
-      l_w: [ 1e-5, 1e-2 ]
+      num_layers: 2
+      num_routings: 3
+      factors: 64
+      factors_multimod: 128
+      batch_size: 1024
+      aggregation: add
+      weight_mode: confid
+      pruning: True
+      has_act: False
+      fusion_mode: concat
+      modalities: ('visual', 'textual')
+      l_w: [1e-5, 1e-2]
+      seed: 123
+    external.LATTICE:
+      meta:
+        hyper_opt_alg: grid
+        verbose: True
+        save_weights: False
+        save_recs: False
+        validation_rate: 10
+        validation_metric: Recall@20
+        restore: False
+      epochs: 200
+      batch_size: 1024
+      factors: 64
+      lr: [0.0001, 0.0005, 0.001, 0.005, 0.01]
+      l_w: [1e-5, 1e-2]
       n_layers: 1
       n_ui_layers: 2
-      top_k: 10
+      top_k: 20
+      l_m: 0.7
       factors_multimod: 64
       modalities: ('visual', 'textual')
-      loaders: ('VisualAttribute', 'TextualAttribute')
-      mw: (0.1,0.9)
-      drop: 0.8
-      lr_sched: (1.0,50)
-      batch_size: 1024
       seed: 123
-      
 """
-
 
 config_ducho = """dataset_path: ./local/data/demo_{dataset}
 gpu list: 0
@@ -157,7 +245,7 @@ experiment:
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--dataset', choices=['baby', 'office', 'music'], help="Dataset name.", required=True)
+    parser.add_argument('--dataset', choices=['baby', 'office', 'music', 'toys', 'beauty'], help="Dataset name.", required=True)
     parser.add_argument('--batch_size', type=int, help="Batch size.", required=True)
 
 
